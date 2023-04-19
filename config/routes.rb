90757d13081c65a27861b0f45a4da881962efea6
drfudd2001 <elmer.figueroagarcia@gmail.com>
2023-04-19 15:45:52 -0700

    Story 1, 2 and 3 completed

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'blogs' => 'blog#index', as: 'blogs'
 
+  get 'blogs/new' => 'blog#new', as: 'new_blog'
+
   get 'blogs/:id' => 'blog#show', as: 'blog'
 
 end
