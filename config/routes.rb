6959735082f6dee698ccae8c84ae61735b91107f
drfudd2001 <elmer.figueroagarcia@gmail.com>
2023-04-19 15:26:34 -0700

    Story 1 and 2 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'blog#index'
 
-  get 'blogs' => 'blog#index'
+  get 'blogs' => 'blog#index', as: 'blogs'
+
+  get 'blogs/:id' => 'blog#show', as: 'blog'
+
 end
