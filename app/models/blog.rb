4e8da549e44ac54145eda9b2b010a44599418761
raymondkneipp <raymondkneipp@gmail.com>
2023-04-20 10:17:27 -0700

    add validation to blog post

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ApplicationRecord
+  validates :title, :content, presence: true
+  validates :title, length: { in: 4..45 }
+  validates :content, length: { in: 10..255 }
+  validates :title, uniqueness: true
 end
